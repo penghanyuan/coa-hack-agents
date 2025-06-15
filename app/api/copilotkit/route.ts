0dd6b9a3f45f4c5218dfc340a3f2accc97619bf9
penghanyuan <penghanyuan@gmail.com>
2025-06-15 11:14:58 +0200

    init

---
+++ app/api/copilotkit/route.ts
@@ -0,0 +1,24 @@
+import {
+  CopilotRuntime,
+  OpenAIAdapter,
+  copilotRuntimeNextJSAppRouterEndpoint,
+} from "@copilotkit/runtime";
+import { NextRequest } from "next/server";
+import OpenAI from "openai";
+
+const openai = new OpenAI();
+const serviceAdapter = new OpenAIAdapter({ openai });
+
+const runtime = new CopilotRuntime({
+  remoteEndpoints: [{ url: "http://localhost:8000/copilotkit_remote" }],
+});
+
+export const POST = async (req: NextRequest) => {
+  const { handleRequest } = copilotRuntimeNextJSAppRouterEndpoint({
+    runtime,
+    serviceAdapter,
+    endpoint: req.nextUrl.pathname,
+  });
+
+  return handleRequest(req);
+};
